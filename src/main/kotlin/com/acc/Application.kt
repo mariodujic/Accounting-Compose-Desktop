8a3e95fb2dff2bb4eed0886daac563f206545e5e
Mario Dujić <mario.dujic@gmail.com>
2022-03-05 07:09:37 +0100

    Initial commit

---
+++ src/main/kotlin/com/acc/Application.kt
@@ -0,0 +1,10 @@
+package com.acc
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        Main()
+    }
+}
