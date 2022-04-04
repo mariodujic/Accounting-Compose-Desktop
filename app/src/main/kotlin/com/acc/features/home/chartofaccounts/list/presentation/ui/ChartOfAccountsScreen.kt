fab087e374bcf289ffd4e80b54ffd3b038c3acab
Mario Dujić <mario.dujic@gmail.com>
2022-04-04 17:52:47 +0200

    Add ChartOfAccount action buttons animation

--- app/src/main/kotlin/com/acc/features/home/chartofaccounts/list/presentation/ui/ChartOfAccountsScreen.kt
+++ app/src/main/kotlin/com/acc/features/home/chartofaccounts/list/presentation/ui/ChartOfAccountsScreen.kt
@@ -1,11 +1,15 @@
 package com.acc.features.home.chartofaccounts.list.presentation.ui
 
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.*
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
@@ -15,8 +19,9 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.acc.common.components.AppIcon
-import com.acc.common.ui.mediumPadding
+import com.acc.common.ui.smallPadding
 import com.acc.features.home.chartofaccounts.list.presentation.viewmodel.ChartOfAccountsViewModel
 import com.acc.features.home.navigation.CharOfAccounts
 import com.navigation.produce
@@ -30,6 +35,8 @@ fun ChartOfAccountsScreen(
     val accounts by viewModel.accounts.collectAsState(initial = emptyList())
     val selectedAccount by viewModel.selectedAccount.collectAsState()
 
+    val rowHeight = 40.dp
+
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(onClick = navigateAddAccount) {
@@ -39,12 +46,16 @@ fun ChartOfAccountsScreen(
     ) {
         LazyColumn {
             itemsIndexed(accounts) { index, item ->
+
+                val width by animateDpAsState(if (selectedAccount?.id == item.id) rowHeight else 0.dp)
+
                 Row(
                     verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(mediumPadding),
+                    horizontalArrangement = Arrangement.spacedBy(smallPadding),
                     modifier = Modifier
                         .clickable { viewModel.selectAccount(item) }
                         .fillMaxWidth()
+                        .height(rowHeight)
                         .background(
                             MaterialTheme.colors.surface.copy(
                                 if (selectedAccount?.id == item.id) 0.1f
@@ -53,18 +64,27 @@ fun ChartOfAccountsScreen(
                             )
 
                         )
-                        .padding(mediumPadding)
                 ) {
-                    Text(text = item.number)
+                    Text(text = item.number, modifier = Modifier.padding(start = smallPadding))
                     Text(text = item.description)
                     Spacer(modifier = Modifier.weight(1f))
-                    if (selectedAccount?.id == item.id) {
-                        IconButton(onClick = {}) {
-                            AppIcon(Icons.Default.Edit)
-                        }
-                        IconButton(onClick = {}) {
-                            AppIcon(Icons.Default.Delete)
-                        }
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .clickable { }
+                            .width(width)
+                    ) {
+                        AppIcon(Icons.Default.Edit)
+                    }
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .clickable { }
+                            .width(width)
+                    ) {
+                        AppIcon(Icons.Default.Delete)
                     }
                 }
             }
